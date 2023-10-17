19e3128090de1f05c2d01d40c54a7f7cb374fd3f
taqi <taqi@gmail.com>
2023-10-17 12:01:49 +0430

    get optimized images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,7 +5,16 @@ interface Props {
 }
 const CriticScore = ({ score }: Props) => {
   const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
-  return <Badge colorScheme={color}>{score}</Badge>;
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
 };
 
 export default CriticScore;
